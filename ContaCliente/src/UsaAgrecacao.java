1cd92652c9c51799156f18ed9b49af09f8947131
Gabriel Romanovski Vitorasse <vitorassegabriel@gmail.com>
2026-03-16 08:45:27 -0300

    Add files via upload

---
+++ ContaCliente/src/UsaAgrecacao.java
@@ -0,0 +1,19 @@
+
+void main(){
+    cliente cli01 = new cliente("Vinicius", "UVV", "000.000.000-00");
+    conta conta01 = new conta(1056, 10000.0);
+    
+    conta01.c = cli01;
+    System.out.println("Titular da conta "+conta01.numero+": "+conta01.c.nome+".");
+    
+    conta01.saca(2000);
+    conta01.verificarSaldo();
+    conta01.deposita(2000);
+    conta01.verificarSaldo();
+    conta01.transfere();
+    
+    //System.out.println("Nome do cliente: "+cli01.nome);
+    
+
+    
+}
