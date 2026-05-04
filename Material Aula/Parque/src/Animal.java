eb263f793aa5f73c3ac573dc36b8e535fe1b538d
Gabriel Romanovski Vitorasse <vitorassegabriel@gmail.com>
2026-05-04 06:40:24 -0300

    Edição - PC

--- Material Aula/Parque/src/Animal.java
+++ Material Aula/Parque/src/Animal.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author alunolab10
- */
 public class Animal extends SerVivo{
     String sexo;
     int numeroPatas;
